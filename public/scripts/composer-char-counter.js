b50e422fd86ffac9c6634e7c6198a0258713c725
0lgaP <perronolga@gmail.com>
2022-02-01 22:22:31 +0000

    composer-char-counter.js is functional - everybody say hurray

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,27 +1,57 @@
 $(document).ready(function() {
-  console.log(`she ready`)
-  $('#myButton').on('click', () => {
-    console.log('button was clicked')
+  
+  const myInput = $('#tweet-text');
+  // 
+
+  $(myInput).on('input', () => {
+
+    let counterInit = myInput.val().length
+    let counterOut = 140 - counterInit
+    if(counterOut >= 0){
+      $('#output').val(counterOut)
+    } else {
+      $('#output').removeClass('counter');
+      $('#output').addClass('counterNegative');
+      $('#output').val(counterOut);
+    }
+
+    
+    
   })
 
-  myButton.on('click', () => {
-    //grab input field
-    const myInput = $('#my-input');
 
-    //grab value the user typed
-    const newText = myInput.val();
 
-    //create a new li with the text from the user
-    const myNewLi = $('<li>').text(newText);
 
-    //add to DOM
-    $('#main-list').append(myNewLi)
+  // $('#myButton').on('click', () => {
+  //   console.log('button was clicked')
 
-    //clear input field
-    myInput.val('');
+  //   const myInput = $('#tweet-text');
 
-    //give focus (click into input field)
-    myInput.focus();
+  //   const newText = myInput.val();
+
+  //   console.log(this)
+  // })
+
+
+
+  // myButton.on('click', () => {
+  //   //grab input field
+  //   const myInput = $('#my-input');
+
+  //   //grab value the user typed
+  //   const newText = myInput.val();
+
+  //   //create a new li with the text from the user
+  //   const myNewLi = $('<li>').text(newText);
+
+  //   //add to DOM
+  //   $('#main-list').append(myNewLi)
+
+  //   //clear input field
+  //   myInput.val('');
+
+  //   //give focus (click into input field)
+  //   myInput.focus();
 
     //to set enter - it has somehting do do with the form
 
@@ -30,4 +60,3 @@ $(document).ready(function() {
 
 
   
-});
