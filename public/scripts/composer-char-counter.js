d554ed1be24f803a6ce4beb2a4dad0f956ec2a7b
0lgaP <perronolga@gmail.com>
2022-02-01 18:56:11 +0000

    created new js fro composer-char-counter and adjusted tweet css to comply with compass design

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,4 +1,31 @@
 $(document).ready(function() {
+  console.log(`she ready`)
+  $('#myButton').on('click', () => {
+    console.log('button was clicked')
+  })
+
+  myButton.on('click', () => {
+    //grab input field
+    const myInput = $('#my-input');
+
+    //grab value the user typed
+    const newText = myInput.val();
+
+    //create a new li with the text from the user
+    const myNewLi = $('<li>').text(newText);
+
+    //add to DOM
+    $('#main-list').append(myNewLi)
+
+    //clear input field
+    myInput.val('');
+
+    //give focus (click into input field)
+    myInput.focus();
+
+    //to set enter - it has somehting do do with the form
+
+  })
 
 
 
