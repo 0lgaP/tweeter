25cf51630b1dd09e7bfdf9e5c7192a0ba8d2a12a
0lgaP <perronolga@gmail.com>
2022-02-04 01:23:25 +0000

    clint for client.js

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,50 +4,45 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-
-
-
 $(() => {
 
- //Hide Errors..........................................//
- $('#display-error').hide()
+  //Hide Errors..........................................//
+  $('#display-error').hide();
 
  
- // LOAD TWEETS .......................................................................//
- const loadTweets = () => {
-   $.ajax({
-     url: "/tweets",
-     method: "GET",
-     dataType: "json",
-     success: (tweets) => {
-       console.log("data:", tweets)
-       renderTweets(tweets);
+  // LOAD TWEETS .......................................................................//
+  const loadTweets = () => {
+    $.ajax({
+      url: "/tweets",
+      method: "GET",
+      dataType: "json",
+      success: (tweets) => {
+        console.log("data:", tweets);
+        renderTweets(tweets);
        
       },
       error: (err) => {
-        console.log(`there was an error: ${err}`)
+        console.log(`there was an error: ${err}`);
       }
-    })
-  }
+    });
+  };
   
   loadTweets();
   
   
   //VULNERABILITY.........................................................................//
-//escape only works on the way out, if you put it into the form, you will break her!
-const escape = function (str) {
-  let div = document.createElement("div");
-  div.appendChild(document.createTextNode(str));
-  return div.innerHTML;
-};
+  //escape only works on the way out, if you put it into the form, you will break her!
+  const escape = function(str) {
+    let div = document.createElement("div");
+    div.appendChild(document.createTextNode(str));
+    return div.innerHTML;
+  };
 
-
-
-// CREATE TWEET ELEMENT ..............................................................//
-const createTweetElement = (obj) => {
+  // CREATE TWEET ELEMENT ..............................................................//
+  const createTweetElement = (obj) => {
   
-  const $time = timeago.format(obj.created_at);
-  const $tweet = `<div class="tweet-container">
+    const $time = timeago.format(obj.created_at);
+    const $tweet = `<div class="tweet-container">
   <div>
   <article class="icon-holder">
   <div class="credentials">
@@ -68,76 +63,76 @@ const createTweetElement = (obj) => {
   <i class="fa-solid fa-heart"></i>
   </section>
   </footer>
-  </div>`
-  return $tweet
-}
+  </div>`;
+    return $tweet;
+  };
 
 
-// RENDER TWEETS .......................................................................//
+  // RENDER TWEETS .......................................................................//
 
-const renderTweets = (tweets) => {
+  const renderTweets = (tweets) => {
   // clear out tweet container
-  const $tweetContainer = $(".old-tweet");
-  $tweetContainer.empty();
+    const $tweetContainer = $(".old-tweet");
+    $tweetContainer.empty();
   
-  // repopulate old-tweet-container
-  for( const tweet of tweets) {
-    const $tweet = createTweetElement(tweet);
-    $tweetContainer.prepend($tweet);
-  }
-}
+    // repopulate old-tweet-container
+    for (const tweet of tweets) {
+      const $tweet = createTweetElement(tweet);
+      $tweetContainer.prepend($tweet);
+    }
+  };
 
-//Access the form and save it as a jquery-esque const//
-const $form = $("#new-tweet-form");
+  //Access the form and save it as a jquery-esque const//
+  const $form = $("#new-tweet-form");
 
   
   // ON SUBMIT ...........................................................................//
   const showError = (msg) => {
     //no appending - we append 5ever
 
-    return $('#display-error').html(msg)
-  }
+    return $('#display-error').html(msg);
+  };
 
   //Strech.................................................//
   
 
 
   $('.invisible').on("click", function(event) {
-    $('.new-tweet-container').toggle("slow")
-  })
+    $('.new-tweet-container').toggle("slow");
+  });
   
 
   $form.on("submit", function(event) {
     event.preventDefault();
-    const $tweetx = $('#tweet-text')
-    const tweetlength = $tweetx.val().length; 
+    const $tweetx = $('#tweet-text');
+    const tweetlength = $tweetx.val().length;
     
     const serializedData = $(this).serialize();
     
     if ($tweetx.val() === "" || $tweetx.val() === null) {
-      const errorShow = showError("&#x1f64a"+ " Please type some words " + "&#x1f64a")
-      $('#display-error').show("slow").slideDown()
+      showError("&#x1f64a" + " Please type some words " + "&#x1f64a");
+      $('#display-error').show("slow").slideDown();
       $('#tweet-text').on('focus', () => {
-        $('#display-error').hide()
-      })
+        $('#display-error').hide();
+      });
     }
     
-    if(tweetlength > 140) {
-      const errorShow = showError("&#128585" + " This story is too long " + "&#128585")
-      $('#display-error').show("slow").slideDown()
+    if (tweetlength > 140) {
+      showError("&#128585" + " This story is too long " + "&#128585");
+      $('#display-error').show("slow").slideDown();
       $('#tweet-text').on('focus', () => {
-        $('#display-error').hide()
-      })
-    } 
+        $('#display-error').hide();
+      });
+    }
     
-    if(tweetlength <= 140) {
+    if (tweetlength <= 140) {
       $.post("/tweets", serializedData, (response) => {
         $tweetx.val("").empty();
         console.log(response);
         console.log('form was submitted');
-        loadTweets()
-      })
+        loadTweets();
+      });
     }
-  })
+  });
   
 });
